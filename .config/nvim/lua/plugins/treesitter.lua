ab6b491b6f3f0daa240e1f4acc0f4eaabfd1bb34
Nhan Nguyen - Edward <48123439+edwardnguyen225@users.noreply.github.com>
2023-11-19 16:59:04 +0700

    feat: Update Neovim setup

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,22 @@
+return {
+  -- { "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "cmake",
+        "css",
+        "gitcommit",
+        "gitignore",
+        "go",
+        "graphql",
+        "http",
+        "php",
+        "scss",
+        "sql",
+        "svelte",
+      })
+    end,
+  },
+}
