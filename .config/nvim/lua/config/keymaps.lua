db92322b5464842a985fa4f7a419a3d06c40e580
Nhan Nguyen - Edward <48123439+edwardnguyen225@users.noreply.github.com>
2023-11-19 16:42:38 +0700

    feat(nvim): Add new keymaps, and options
    This commit adds keymap "JJ" and "JK" to quickly escape.
    
    Besides, it makes the winbar show up.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+
+vim.keymap.set("i", "jj", "<ESC>", { silent = true })
+vim.keymap.set("i", "jk", "<ESC>", { silent = true })
