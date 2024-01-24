173cc1d8996ededf60dba75986663e7731b42272
Nhan Nguyen - Edward <48123439+edwardnguyen225@users.noreply.github.com>
2024-01-24 16:19:40 +0700

    feat(eslint): Add eslint as formatter

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -15,4 +15,25 @@ return {
       })
     end,
   },
+
+  -- If your project is using eslint with eslint-plugin-prettier,
+  -- then this will automatically fix eslint errors and format with prettier on save.
+  -- Important: make sure not to add prettier to null-ls, otherwise this won't work!
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = { eslint = {} },
+      setup = {
+        eslint = function()
+          require("lazyvim.util").lsp.on_attach(function(client)
+            if client.name == "eslint" then
+              client.server_capabilities.documentFormattingProvider = true
+            elseif client.name == "tsserver" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end)
+        end,
+      },
+    },
+  },
 }
