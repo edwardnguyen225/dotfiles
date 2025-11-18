2ee2f1e48c25dc68023e3cb72facc1488424b385
Edward <48123439+edwardnguyen225@users.noreply.github.com>
2025-11-18 08:43:30 +0700

    feat: update lazyvim

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -23,7 +23,7 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
-      servers = { 
+      servers = {
         eslint = {},
         svelte = {
           keys = {
@@ -37,13 +37,20 @@ return {
       },
       setup = {
         eslint = function()
-          require("lazyvim.util").lsp.on_attach(function(client)
-            if client.name == "eslint" then
-              client.server_capabilities.documentFormattingProvider = true
-            elseif client.name == "tsserver" then
-              client.server_capabilities.documentFormattingProvider = false
-            end
-          end)
+          vim.api.nvim_create_autocmd("LspAttach", {
+            callback = function(args)
+              local client = vim.lsp.get_client_by_id(args.data.client_id)
+              if not client then
+                return
+              end
+
+              if client.name == "eslint" then
+                client.server_capabilities.documentFormattingProvider = true
+              elseif client.name == "tsserver" or client.name == "vtsls" then
+                client.server_capabilities.documentFormattingProvider = false
+              end
+            end,
+          })
         end,
       },
     },
