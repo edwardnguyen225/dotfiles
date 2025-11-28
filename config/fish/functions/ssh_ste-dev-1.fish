2e0832d93e771597be312bae4b9b3613d827c7f3
Edward - Nhan Nguyen <48123439+edwardnguyen225@users.noreply.github.com>
2025-11-28 19:50:45 +0700

    chore: remove ssh function; update karabiner mapping and enable tmux restore

--- config/fish/functions/ssh_ste-dev-1.fish
+++
@@ -1,7 +0,0 @@
-function ssh_ste-dev-1
-    set ssh_file ~/.ssh/id_ste-dev-1
-    ssh-agent -k
-
-    eval $(ssh-agent -c) && ssh-add --apple-use-keychain $ssh_file
-    echo 'New SSH is added with STE SSH keychain'
-end
